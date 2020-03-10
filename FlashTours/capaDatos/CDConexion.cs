5a69304e4faec6a808959ba92118b28574402f03
Denys7u7 <dennyscruz20@gmail.com>
2020-03-10 01:22:09 -0600

    0001

---
+++ FlashTours/capaDatos/CDConexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDatos
+{
+    class CDConexion
+    {
+        private SqlConnection Conexion = new SqlConnection(@"Server=.\SQLExpress;DataBase=ped;" + "Trusted_Connection=True;MultipleActiveResultSets=true; Persist Security Info=True");
+
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
---
+++ FlashTours/capaDatos/CDUsuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace capaDatos
+{
+    class CDUsuario
+    {
+
+        private CDConexion conexion = new CDConexion();
+        private SqlDataReader leer;
+
+        public SqlDataReader logearse(string user, string pass)
+        {
+
+
+            SqlCommand comando = new SqlCommand("logearse", conexion.AbrirConexion());
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@usuario", user);
+            comando.Parameters.AddWithValue("@pass", pass);
+            leer = comando.ExecuteReader();
+            comando.Parameters.Clear();
+            return leer;
+        }
+    }
+}
